901f9a912f9544e2614e480615b430192c8ee42d
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2022-01-27 22:12:26 -0300

    .

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useContext } from "react/cjs/react.development";
+import React, {useContext} from "react";
 import { PokemonContext } from "../../contexts/PokemonContext";
 
 import "./Card.css";
