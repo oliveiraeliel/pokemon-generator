d62fef22b6a31ea6f9080d18dd1f82374d034149
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2022-01-27 21:49:19 -0300

    Arrumando

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -36,35 +36,28 @@ export default function Button() {
       .then((r) => r.json())
       .then((json) => {
         setPokemon(json);
-        if (!pokemon.sprites.other.dream_world.front_default) handleClick();
-        else {
-          setImg(pokemon.sprites.other.dream_world.front_default);
-          setAbility(pokemon.abilities[0].ability.name.capitalize());
-          setName(pokemon.name.capitalize());
-          setHp(pokemon.stats[0].base_stat)
-          setDefense(pokemon.stats[1].base_stat)
-          setAtack(pokemon.stats[2].base_stat)
-          setSAtack(pokemon.stats[3].base_stat)
-          setSDefense(pokemon.stats[4].base_stat)
-          setSpeed(pokemon.stats[5].base_stat)
-          // console.log(pokemon.stats[0].base_stat)
-          // console.log(pokemon.stats[1].base_stat)
-          // console.log(pokemon.stats[2].base_stat)
-          // console.log(pokemon.stats[3].base_stat)
-          // console.log(pokemon.stats[4].base_stat)
-          // console.log(pokemon.stats[5].base_stat)
-
-
-
-          setType(pokemon.types[0].type.name.capitalize());
-          if (pokemon.types[1])
-            setType(
-              pokemon.types[0].type.name.capitalize() +
-                " + " +
-                pokemon.types[1].type.name.capitalize()
-            );
+        if (pokemon.sprites.other.dream_world.front_default === null) {
+          handleClick();
+          return;
         }
-        console.log(pokemon);
+
+        setImg(pokemon.sprites.other.dream_world.front_default);
+        setAbility(pokemon.abilities[0].ability.name.capitalize());
+        setName(pokemon.name.capitalize());
+        setHp(pokemon.stats[0].base_stat);
+        setDefense(pokemon.stats[1].base_stat);
+        setAtack(pokemon.stats[2].base_stat);
+        setSAtack(pokemon.stats[3].base_stat);
+        setSDefense(pokemon.stats[4].base_stat);
+        setSpeed(pokemon.stats[5].base_stat);
+
+        setType(pokemon.types[0].type.name.capitalize());
+        if (pokemon.types[1])
+          setType(
+            pokemon.types[0].type.name.capitalize() +
+              " + " +
+              pokemon.types[1].type.name.capitalize()
+          );
       });
   }, [pokeId]);
 
